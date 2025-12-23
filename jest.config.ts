6f9530242e7448bdf214c0e5abed33faed155e58
Luiz Resplande <resplandeluiz@Mac-mini-de-Luiz.local>
2025-12-23 07:41:50 -0300

    adicionando nome ao job

--- jest.config.ts
+++ jest.config.ts
@@ -1,38 +1,38 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
 
-  rootDir: '.',
+    rootDir: '.',
 
-  moduleFileExtensions: ['ts', 'js', 'json'],
+    moduleFileExtensions: ['ts', 'js', 'json'],
 
-  testMatch: ['**/*.spec.ts'],
+    testMatch: ['**/*.spec.ts'],
 
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1',
-  },
-
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-
-  collectCoverageFrom: [
-    'src/**/*.{ts,js}',
-    '!src/**/*.spec.{ts,js}',
-    '!src/**/*.test.{ts,js}',
-    '!src/**/index.{ts,js}',
-  ],
-
-  coverageThreshold: {
-    global: {
-      branches: 60,
-      functions: 60,
-      lines: 60,
-      statements: 60,
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
     },
-  },
+
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov', 'text-summary', 'json-summary'],
+
+    collectCoverageFrom: [
+        'src/**/*.{ts,js}',
+        '!src/**/*.spec.{ts,js}',
+        '!src/**/*.test.{ts,js}',
+        '!src/**/index.{ts,js}',
+    ],
+
+    coverageThreshold: {
+        global: {
+            branches: 60,
+            functions: 60,
+            lines: 60,
+            statements: 60,
+        },
+    },
 };
 
 export default config;
