2822d862a41d76fdb3c7a03e90834f7eb5dd45fc
Luiz Resplande <resplandeluiz@Mac-mini-de-Luiz.local>
2025-12-23 09:03:29 -0300

    fix: adicionando teste para passar nos 80%

--- src/order/presenters/mappers/order-presentation.mapper.spec.ts
+++ src/order/presenters/mappers/order-presentation.mapper.spec.ts
@@ -21,26 +21,82 @@ describe('OrderPresentationMapper', () => {
   } as unknown as Order;
 
   it('should map order item to interface', () => {
-    OrderPresentationMapper.mapOrderItemToInterface(mockOrderItem);
+    const result = OrderPresentationMapper.mapOrderItemToInterface(mockOrderItem);
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty('itemId', mockOrderItem.itemId);
+    expect(result).toHaveProperty('quantity', mockOrderItem.quantity);
+    expect(result).toHaveProperty('price', mockOrderItem.price);
+    expect(result).toHaveProperty('itemQuantity', mockOrderItem.quantity);
   });
 
   it('should map order items to interface', () => {
-    OrderPresentationMapper.mapOrderItemsToInterface([mockOrderItem]);
+    const result = OrderPresentationMapper.mapOrderItemsToInterface([mockOrderItem]);
+
+    expect(result).toBeDefined();
+    expect(Array.isArray(result)).toBe(true);
+    expect(result).toHaveLength(1);
+
+
+    const item = result[0];
+    expect(item).toHaveProperty('itemId', mockOrderItem.itemId);
+    expect(item).toHaveProperty('quantity', mockOrderItem.quantity);
+    expect(item).toHaveProperty('price', mockOrderItem.price);
+    expect(item).toHaveProperty('itemQuantity', mockOrderItem.quantity);
   });
 
   it('should map order to interface', () => {
-    OrderPresentationMapper.mapOrderToInterface(mockOrder);
+    const result = OrderPresentationMapper.mapOrderToInterface(mockOrder);
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty('id', mockOrder.id);
+    expect(result).toHaveProperty('status', mockOrder.status);
+    expect(result).toHaveProperty('totalAmount', mockOrder.totalAmount);
+    expect(result).toHaveProperty('createdAt', mockOrder.createdAt);
+    expect(result).toHaveProperty('updatedAt', mockOrder.updatedAt);
+    expect(result).toHaveProperty('customerId', mockOrder.customerId);
+
+
+    expect(result).toHaveProperty('orderItems');
+    expect(Array.isArray(result.orderItems)).toBe(true);
+    expect(result.orderItems).toHaveLength(mockOrder.orderItems.length);
+
+    const item = result.orderItems[0];
+    expect(item).toHaveProperty('itemId', mockOrder.orderItems[0].itemId);
+    expect(item).toHaveProperty('quantity', mockOrder.orderItems[0].quantity);
+    expect(item).toHaveProperty('price', mockOrder.orderItems[0].price);
+    expect(item).toHaveProperty('itemQuantity', mockOrder.orderItems[0].quantity);
   });
 
   it('should map orders to interface', () => {
-    OrderPresentationMapper.mapOrdersToInterface([mockOrder]);
+    const result = OrderPresentationMapper.mapOrdersToInterface([mockOrder]);
+
+    expect(result).toBeDefined();
+    expect(Array.isArray(result)).toBe(true);
+    expect(result).toHaveLength(1);
+
+    const mappedOrder = result[0];
+
+    expect(mappedOrder).toHaveProperty('id', mockOrder.id);
+    expect(mappedOrder).toHaveProperty('status', mockOrder.status);
+    expect(mappedOrder).toHaveProperty('totalAmount', mockOrder.totalAmount);
+    expect(mappedOrder).toHaveProperty('customerId', mockOrder.customerId);
+
+    expect(mappedOrder).toHaveProperty('orderItems');
+    expect(Array.isArray(mappedOrder.orderItems)).toBe(true);
+    expect(mappedOrder.orderItems).toHaveLength(mockOrder.orderItems.length);
   });
 
   it('should map order item dto to interface', () => {
-    OrderPresentationMapper.mapOrderItemDtoToInterface({
+    const orderItemDTO = {
       itemId: 'item-1',
       itemQuantity: 2,
-    });
+    }
+
+    const result = OrderPresentationMapper.mapOrderItemDtoToInterface(orderItemDTO);
+
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty('itemId', mockOrderItem.itemId);
+    expect(result).toHaveProperty('quantity', mockOrderItem.quantity);
+    expect(result).toHaveProperty('itemQuantity', mockOrderItem.quantity);
   });
 
   it('should map order dto to interface', () => {
@@ -54,26 +110,74 @@ describe('OrderPresentationMapper', () => {
       ],
     };
 
-    OrderPresentationMapper.mapOrderDtoToInterface(dto);
+    const result = OrderPresentationMapper.mapOrderDtoToInterface(dto);
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty('customerCpf', dto.customerCpf);
+    expect(result).toHaveProperty('orderItems');
+    expect(Array.isArray(result.orderItems)).toBe(true);
+    expect(result.orderItems).toHaveLength(dto.orderItems.length);
   });
 
   it('should map order to api response', () => {
-    OrderPresentationMapper.mapOrderToApiResponse(mockOrder);
+    const result = OrderPresentationMapper.mapOrderToApiResponse(mockOrder);
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty('id', mockOrder.id);
+    expect(result).toHaveProperty('status', mockOrder.status);
+    expect(result).toHaveProperty('totalAmount', mockOrder.totalAmount.toFixed(2));
+    expect(result).toHaveProperty('createdAt', mockOrder.createdAt.toISOString());
+    expect(result).toHaveProperty('updatedAt', mockOrder.updatedAt.toISOString());
+    expect(result).toHaveProperty('customerId', mockOrder.customerId);
+
+    expect(result).toHaveProperty('orderItems');
+    expect(Array.isArray(result.orderItems)).toBe(true);
+    expect(result.orderItems).toHaveLength(mockOrder.orderItems.length);
   });
 
   it('should map order to summary', () => {
-    OrderPresentationMapper.mapOrderToSummary(mockOrder);
+    const result = OrderPresentationMapper.mapOrderToSummary(mockOrder);
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty('id', mockOrder.id);
+    expect(result).toHaveProperty('status', mockOrder.status);
+    expect(result).toHaveProperty('totalAmount', mockOrder.totalAmount);
+    expect(result).toHaveProperty('createdAt', mockOrder.createdAt);
+    expect(result).toHaveProperty('customerId', mockOrder.customerId);
   });
 
   it('should map orders to summary', () => {
-    OrderPresentationMapper.mapOrdersToSummary([mockOrder]);
+    const mock = [mockOrder]
+    const result = OrderPresentationMapper.mapOrdersToSummary(mock);
+
+    expect(Array.isArray(result)).toBe(true);
+    expect(result).toHaveLength(mock.length);
+
+    const mappedSummary = result[0];
+
+    expect(mappedSummary).toBeDefined();
+    expect(mappedSummary).toHaveProperty('id', mockOrder.id);
+    expect(mappedSummary).toHaveProperty('status', mockOrder.status);
+    expect(mappedSummary).toHaveProperty('totalAmount', mockOrder.totalAmount);
+    expect(mappedSummary).toHaveProperty('createdAt', mockOrder.createdAt);
+    expect(mappedSummary).toHaveProperty('customerId', mockOrder.customerId);
   });
 
   it('should format currency', () => {
-    OrderPresentationMapper.formatCurrency(100);
+    const mock = 100
+    const result = OrderPresentationMapper.formatCurrency(mock);
+    expect(result.replace(/\s/g, ' ')).toBe('R$ 100,00');
   });
 
   it('should map order to display format', () => {
-    OrderPresentationMapper.mapOrderToDisplayFormat(mockOrder);
+    const result = OrderPresentationMapper.mapOrderToDisplayFormat(mockOrder);
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty('id', mockOrder.id);
+    expect(result).toHaveProperty('status', mockOrder.status);
+    expect(result).toHaveProperty('totalAmount');
+    expect(result).toHaveProperty('createdAt');
+    expect(result).toHaveProperty('updatedAt');
+    expect(result).toHaveProperty('customerId', mockOrder.customerId);
+
+    expect(result).toHaveProperty('orderItems');
+    expect(Array.isArray(result.orderItems)).toBe(true);
+    expect(result.orderItems).toHaveLength(mockOrder.orderItems.length);
   });
 });
