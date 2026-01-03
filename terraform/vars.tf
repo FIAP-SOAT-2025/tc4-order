2154ff17918bfec8f72c5a31286c7cc831c2e8d3
Daniela-Rego <danielalimaqueiroz@gmail.com>
2026-01-03 13:01:18 -0300

    ajustes vars

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -47,17 +47,17 @@ variable "db_name" {
 variable "item_service_url" {
   description = "URL do serviço de Items"
   type        = string
-  default     = "http://api-service.tc4-item.svc.cluster.local"
+  default     = "http://api-service.tc4-item.svc.cluster.local:8080"
 }
 
 variable "customer_service_url" {
   description = "URL do serviço de Customers"
   type        = string
-  default     = "http://api-service.tc4-customer.svc.cluster.local"
+  default     = "http://api-service.tc4-customer.svc.cluster.local:8080"
 }
 
 variable "payment_service_url" {
   description = "URL do serviço de Payments"
   type        = string
-  default     = "http://api-service.tc4-payments.svc.cluster.local"
+  default     = "http://api-service.tc4-payments.svc.cluster.local:8080"
 }
