b962b3a8139d1f06cb7abbe01e509b60caba9ca0
Beibão (Felipe Alves) <felipe.teixeira@luizalabs.com>
2026-01-04 12:29:08 -0300

    ajustando conexao com os services

--- src/order/infraestructure/external/item/item.client.ts
+++ src/order/infraestructure/external/item/item.client.ts
@@ -9,7 +9,7 @@ export class ItemClient implements ItemClientInterface {
     private readonly api: AxiosInstance;
     constructor() {
         this.api = axios.create({
-            baseURL: "http://api-service.tc4-item.svc.cluster.local:8080",
+            baseURL: "http://api-service-internal.tc4-item.svc.cluster.local",
             headers: {
                 "Content-Type": "application/json",
             },
